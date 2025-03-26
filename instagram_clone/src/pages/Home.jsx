decf1f92f96b2458660122699be95854c5a6cd8e
CJay-Cipher <chijiokennabueze@gmail.com>
2025-03-26 11:24:37 +0100

    input error handling

--- instagram_clone/src/pages/Home.jsx
+++ instagram_clone/src/pages/Home.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
+  const [showError, setShowError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (username) {
       navigate(`/profile/${username}`);
+      setShowError(false);
+    } else {
+      setShowError(true);
     }
   };
 
@@ -16,7 +20,17 @@ const Home = () => {
     <main className="home-main">
       <div className="home-container">
         <form onSubmit={handleSubmit}>
-          <input type="text" className="profile-input" placeholder="Enter Instagram Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          {showError && <p className="input-error shaker">Please enter a valid Username.</p>}
+          <input
+            type="text"
+            className="profile-input"
+            placeholder="Enter Instagram Username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setShowError(false);
+            }}
+          />
           <button type="submit" className="get-profile-button">
             View Profile
           </button>
